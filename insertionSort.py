30613f9290526fcae36ed1fc8d911296c26eeb07
JUYUN PARK <c.park2026@calvinschool.kr>
2025-12-01 22:50:17 +0900

    Create insertionSort.py

---
+++ insertionSort.py
@@ -0,0 +1,10 @@
+arr = [12, 11, 13, 5, 6]
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while arr[j - 1] > arr[j] and j > 0:
+            arr[j - 1], arr[j] = arr[j], arr[j-1]
+            j -= 1
+
+insertion_sort(arr)
